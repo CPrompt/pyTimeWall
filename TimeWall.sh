959613ab83b9d8733185c65c30e7dabe266892d4
CPrompt <curtadkins@gmail.com>
2015-02-22 17:58:27 -0500

    Added a README file with some setup instruction and edited the TimeWall.sh file

--- TimeWall.sh
+++ TimeWall.sh
@@ -2,7 +2,7 @@
 pid=$(ps -C xfce4-session -o pid=)
 pid=$(echo $pid)
 
-
 export DBUS_SESSION_BUS_ADDRESS=$(grep -z DBUS_SESSION_BUS_ADDRESS /proc/$pid/environ|cut -d= -f2-)
 
-python /home/curtis/Scripts/time_wall_changer/pyTimeWall.py
+# You need to edit this to match the path of pyTimeWall.py
+python /home/user/pyTimeWall.py
