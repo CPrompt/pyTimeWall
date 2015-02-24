ffc6a3c61eaefebb6aa693b6830933696bf3f768
CPrompt <curtadkins@gmail.com>
2015-02-23 21:58:44 -0500

    Added a generic path to the pyTimeWall.py in the bash script

--- TimeWall.sh
+++ TimeWall.sh
@@ -5,4 +5,4 @@ pid=$(echo $pid)
 export DBUS_SESSION_BUS_ADDRESS=$(grep -z DBUS_SESSION_BUS_ADDRESS /proc/$pid/environ|cut -d= -f2-)
 
 # You need to edit this to match the path of pyTimeWall.py
-python /home/user/pyTimeWall.py
+python /home/user/path/to/pyTimeWall.py
